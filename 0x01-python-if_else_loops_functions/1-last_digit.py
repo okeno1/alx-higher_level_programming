52f78ece117eb1a396f7c68f0bfd0231e20898b5
okeno1 <petercoin.1997@gmail.com>
2022-09-20 15:58:58 -0700

    assigning a random number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,41 +1,19 @@
 #!/usr/bin/python3
-
 import random
-
 number = random.randint(-10000, 10000)
-
 RandomNum = number
-
-
-
 if number < 0:
-
     number = -(number)
-
 else:
-
     number = number
-
-
-
     lastDigit = number % 10
-
     if RandomNum < 0:
-
         number = RandomNum
-
         lastDigit = -(lastDigit)
-
         if lastDigit > 5:
-
             string = "and is greater than 5"
-
         elif lastDigit == 0:
-
             string = "and is 0"
-
         elif lastDigit < 6:
-
             string = "and is less than 6 and not 0"
-
         print(f"Last digit of {number} is {lastDigit}", string)
