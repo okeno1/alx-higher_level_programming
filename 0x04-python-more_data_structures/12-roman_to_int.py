dacae50179b77470c5c51c494318977eb000558f
okeno1 <petercoin.1997@gmail.com>
2022-09-28 11:39:54 -0700

    converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
-        return (0)
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_list = list(roman_string.upper())
-    result = 0
-    previous = 0
-    for letter in roman_list:
-        if letter in roman_dict:
-            result += roman_dict[letter]
-            if roman_dict[letter] > previous:
-                result -= previous * 2
-            previous = roman_dict[letter]
-        else:
-            return (0)
-        return (result)
+    roman_list = \
+            {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_keys = list(roman_list)
+    numerals = []
+    if len(roman_string) > 0:
+        for i, j in zip(roman_string, roman_keys):
+            if i == j:
+                numerals.append(roman_list[i])
+        decimals = 0            
+        for i in range(len(numerals)):
+            if i < len(numerals):
+                if numerals[i] < numerals[i + 1]:
+                    decimals = (numerals[i] * -1) + numeral[i + 1]
+                else:
+                    decimals += numeral[i]
+        return decimals
