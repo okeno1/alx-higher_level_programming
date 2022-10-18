643ce24be0563fb71c63e360267fd72daeaaed5b
okeno1 <petercoin.1997@gmail.com>
2022-10-18 11:46:23 -0700

    private instance attributes for width and height of defined class rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,6 +14,15 @@ class Rectangle:
             height: height of the rectangle
         """
 
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
+
         self.width = width
         self.height = height
 
