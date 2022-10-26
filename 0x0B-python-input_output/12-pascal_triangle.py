89ef85f9921cc9a34ce5ec9966e48cc010bac462
okeno1 <petercoin.1997@gmail.com>
2022-10-26 13:48:45 -0700

    Pascals's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module 12-pascal_triangle.
+Returns a list of lists of integers
+representing the Pascal’s triangle of n.
+"""
+
+
+def pascal_triangle(n):
+    """Represents Pascal's Triangle of size n"""
+    if n <= 0:
+        return []
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
