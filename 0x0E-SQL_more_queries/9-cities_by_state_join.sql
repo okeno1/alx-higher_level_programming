41b3f23e2e3502303cc6919220c9146ae73f1130
okeno1 <petercoin.1997@gmail.com>
2023-03-05 13:57:44 +0300

    lists all cities contained in the database hbtn_0d_usa.

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Using join to display info about two tables
+-- Statement to do join query
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
